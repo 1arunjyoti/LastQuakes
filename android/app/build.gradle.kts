3e06a44a5e4bedb8db8c41867dff57c9eaf8bc49
Arunjyoti Debnath <155171660+1arunjyoti@users.noreply.github.com>
2025-09-22 20:56:16 +0530

    pre-final

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -7,6 +10,12 @@ plugins {
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
 
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -35,11 +44,41 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
+            applicationVariants.all(closureOf<com.android.build.gradle.api.ApplicationVariant> {
+                outputs.all {
+                    if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
+                        this.outputFileName = "lastquake-${versionName}-${versionCode}.apk"
+                    }
+                }
+            })
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            //signingConfig = signingConfigs.getByName("debug")
+
+            signingConfig = signingConfigs.getByName("release")
+
+            // Enables code-related app optimization.
+            isMinifyEnabled = true
+            
+            // Enables resource shrinking.
+            isShrinkResources = true
+
+            proguardFiles(
+                // Default file with automatically generated optimization rules.
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+            )
+
         }
     }
 }
